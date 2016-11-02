7980414fb0cee4be11004b4eba89cc074ac415b8
ed-karabinus <ejkarabin@gmail.com>
2016-11-02 15:02:31 -0500

    Started Rails Amusement Park Lab.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,15 @@
 class Ride < ActiveRecord::Base
   # write associations here
+  belongs_to :attraction
+  belongs_to :user
+
+  def take_ride
+    error = 'Sorry.';
+    if self.user.tickets < self.attraction.tickets
+      error += " You do not have enough tickets to ride the #{self.attraction.name}."
+    elsif self.user.height < self.attraction.min_height
+      error += " You are not tall enough to ride the #{self.attraction.name}."
+    end
+    return error
+  end
 end
