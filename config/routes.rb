cd8e97eb7dbac1e92928c469de1f16a65c050e8e
ed-karabinus <ejkarabin@gmail.com>
2016-11-03 11:47:38 -0500

    Working on feature specs.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
 
+  root 'welcome#index'
 
+  resources :users
+  resources :rides
+  resources :attractions
+
+  get '/signin', to: 'sessions#new', as: :new_session
 end
