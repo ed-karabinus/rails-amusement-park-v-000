20c7df37dc6187c45f8d4dae34106189c45278eb
ed-karabinus <ejkarabin@gmail.com>
2016-11-05 12:28:55 -0500

    Still implementing _Go On A Ride_ user flow

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -2,4 +2,8 @@ class AttractionsController < ApplicationController
   def index
     @attractions = Attraction.all
   end
+
+  def show
+    @attraction = Attraction.find_by(id: params[:id])
+  end
 end
