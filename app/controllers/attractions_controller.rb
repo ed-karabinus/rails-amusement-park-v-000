61ce55dada3b93f0ea99fb51c2bd4a4257982440
ed-karabinus <ejkarabin@gmail.com>
2016-11-06 02:13:57 -0600

    All _Go On A Ride_ specs now passing.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -15,15 +15,14 @@ class AttractionsController < ApplicationController
       current_user.nausea += @attraction.nausea_rating
       current_user.happiness += @attraction.happiness_rating
       current_user.save
-      flash[:notice] = "Thanks for riding the #{@attraction.name}!"
-      redirect_to user_path(current_user)
+      flash[:success_notice] = "Thanks for riding the #{@attraction.name}!"
     end
     if current_user.height < @attraction.min_height
-      flash[:alert] = "You are not tall enough to ride the #{@attraction.name}"
+      flash[:height_alert] = "You are not tall enough to ride the #{@attraction.name}"
     end
     if current_user.tickets < @attraction.tickets
-      flash[:alert] = "You do not have enough tickets to ride the #{attraction.name}"
+      flash[:ticket_alert] = "You do not have enough tickets to ride the #{@attraction.name}"
     end
-    redirect_to attractions_path
+    redirect_to user_path(current_user)
   end
 end
